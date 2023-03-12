bce8a4a44c0daaab4612fde153e823c2df37d78f
Thiago Dantas <thiago.dantas@stone.com.br>
2023-03-12 00:45:26 -0300

    feat: create registration and login page

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { View, Text, Platform } from 'react-native';
+
+import { Background, Container, AreaInput, Input, SubmitButton, SubmitText } from './styles';
 
 export default function SignUp() {
   return (
-    <View>
-      <Text>TELA CADASTRO</Text>
-    </View>
+    <Background>
+      <Container behavior={Platform.OS === 'ios' ? 'padding' : ''} enabled>
+        <AreaInput>
+          <Input placeholder="Nome" />
+        </AreaInput>
+
+        <AreaInput>
+          <Input placeholder="Seu email" />
+        </AreaInput>
+
+        <AreaInput>
+          <Input placeholder="Sua senha" />
+        </AreaInput>
+
+        <SubmitButton activeOpacity={0.8}>
+          <SubmitText>Cadastrar</SubmitText>
+        </SubmitButton>
+      </Container>
+    </Background>
   );
 }
