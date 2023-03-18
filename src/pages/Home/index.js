6ba0760d2487c886fb5321ace289024c43ecb377
Thiago Dantas <thiago.dantas@stone.com.br>
2023-03-18 16:02:42 -0300

    feat: initial settings for daily cards

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,42 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { View, Text, Button } from 'react-native';
+import { format } from 'date-fns';
+
 import { AuthContext } from '../../contexts/auth';
+
+import api from '../../services/api';
 
 import Header from '../../components/Header';
 
 import { Background } from './styles.js';
 
 export default function Home() {
+  const [listBalance, setListBalance] = useState([]);
+
+  const [dateMovements, setDateMovements] = useState(new Date());
+
+  useEffect(() => {
+    let isActive = true;
+
+    async function getMovements() {
+      let dateFormated = format(dateMovements, 'dd/MM/yyyy');
+
+      const balance = await api.get('/balance', {
+        params: {
+          date: dateFormated,
+        },
+      });
+
+      if (isActive) {
+        setListBalance(balance.data);
+      }
+    }
+
+    getMovements();
+
+    return () => (isActive = false);
+  }, []);
+
   return (
     <Background>
       <Header title="Minhas movimentações" />
