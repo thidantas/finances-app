f3ee92abbbebdc2a468637996aad697c70879a97
Thiago Dantas <thiago.dantas@stone.com.br>
2023-06-15 00:23:01 -0300

    feat: create profile page

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -3,6 +3,7 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import Home from '../pages/Home';
 import New from '../pages/New';
+import Profile from '../pages/Profile';
 
 const AppDrawer = createDrawerNavigator();
 
@@ -23,7 +24,9 @@ function AppRoutes() {
     >
       <AppDrawer.Screen name="Home" component={Home} />
 
-      <AppDrawer.Screen name="Regristrar" component={New} />
+      <AppDrawer.Screen name="Registrar" component={New} />
+
+      <AppDrawer.Screen name="Perfil" component={Profile} />
     </AppDrawer.Navigator>
   );
 }
